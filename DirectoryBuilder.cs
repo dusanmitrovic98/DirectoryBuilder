b02449048b48976e810e57427af490e4c61af36e
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-05-29 19:50:11 +0200

    Update DirectoryBuilder.cs

--- DirectoryBuilder.cs
+++ DirectoryBuilder.cs
@@ -6,3 +6,4 @@ public class DirectoryTreeCreatorWindow : EditorWindow
 {
     private string baseFolder = "Assets/"; // Specify the base folder here
 
+    private string[] folderPaths = new string[]
